b698a5c20a411b52d9fc1e7eb99ba14582baedb7
Emily Miles <emiles4@gmail.com>
2019-03-07 23:28:10 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,19 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".") then return true
+    if self.end_with?(".")  
+      return true
     else return false
     end
   end
 
   def question?
-    if self.end_with?("?") then return true
+    if self.end_with?("?") 
+      return true
     else return false
     end
   end
 
   def exclamation?
-    if self.end_with?("!") then return true
+    if self.end_with?("!")
+      return true
     else return false
     end
   end
