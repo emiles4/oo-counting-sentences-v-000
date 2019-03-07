b0754e78d0c8e4ad0fab1d888708e2128bfe6aa3
Emily Miles <emiles4@gmail.com>
2019-03-07 23:28:14 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,14 +3,14 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".")  
+    if self.end_with?(".")
       return true
     else return false
     end
   end
 
   def question?
-    if self.end_with?("?") 
+    if self.end_with?("?")
       return true
     else return false
     end
